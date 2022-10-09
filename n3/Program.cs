ce79728967c7f11fd276969015012bff4a7f89bd
sasha <alexandrpudov7@gmail.com>
2022-10-09 19:03:37 +0300

    Добавьте файлы проекта.

---
+++ n3/Program.cs
@@ -0,0 +1,33 @@
+﻿interface IGun
+{
+    void shot();
+}
+
+class Gun : IGun
+{
+    void IGun.shot()
+    {
+        Console.WriteLine("пыщ");
+    }
+}
+class MachineGun : IGun {
+    void IGun.shot()
+    {
+        Console.WriteLine("пыщ, пыщ, пыщ");
+    }
+}
+class n3
+{
+
+    static void Main(){
+        Gun gun = new Gun();
+        MachineGun machine = new MachineGun();
+
+        n(machine);
+        n(gun);
+    }
+    static void n(IGun n)
+    {
+        n.shot();
+    }
+}
